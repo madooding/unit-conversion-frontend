8d972ccae4698f8362f51b7561d0a1da48de04c2
madooding <madooding@gmail.com>
2022-12-21 08:46:28 +0700

    Config: config eslint, prettier, stylelint and tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Test</h1>
+  <h1 class="line-clamp-2">Test</h1>
 </template>
 
 <script lang="ts">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     './components/**/*.{js,vue,ts}',
     './layouts/**/*.vue',
@@ -11,5 +12,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/line-clamp')],
 }
